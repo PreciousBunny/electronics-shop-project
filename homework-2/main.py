01ea739ee87364bc875483729b039d40e7d9f73d
Danila <icy.calmness@gmail.com>
2023-06-21 19:42:52 +0300

    add print() in homework_2.main.py

--- homework-2/main.py
+++ homework-2/main.py
@@ -6,9 +6,11 @@ if __name__ == '__main__':
     # длина наименования товара меньше 10 символов
     item.name = 'Смартфон'
     assert item.name == 'Смартфон'
+    print(item.name)
 
     # длина наименования товара больше 10 символов
     item.name = 'СуперСмартфон'
+    print(item.name)
     # Exception: Длина наименования товара превышает 10 символов.
 
     Item.instantiate_from_csv()  # создание объектов из данных файла
@@ -16,7 +18,11 @@ if __name__ == '__main__':
 
     item1 = Item.all[0]
     assert item1.name == 'Смартфон'
+    print(item1.name)
 
     assert Item.string_to_number('5') == 5
+    print(Item.string_to_number('5'))
     assert Item.string_to_number('5.0') == 5
+    print(Item.string_to_number('5.0'))
     assert Item.string_to_number('5.5') == 5
+    print(Item.string_to_number('5.5'))
