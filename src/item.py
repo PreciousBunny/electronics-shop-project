65a21ff4bdc43e4a6d7a431ce77310b4fb32f22d
Danila <icy.calmness@gmail.com>
2023-06-21 19:39:33 +0300

    add methods for class Item; src.item.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+from src.config import CSV_ITEMS_PATH
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self) -> str:
+        """
+        Метод для доступа к приватному атрибуту с геттером.
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, value) -> None:
+        """
+        Метод устанавливает название товара и проверяет, что длина наименования товара не больше 10 символов.
+        """
+        self.__name = value if len(value) <= 10 else value[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +49,20 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path=CSV_ITEMS_PATH) -> None:
+        """
+        Метод инициализирует экземпляры класса Item данными из CSV-файла.
+        """
+        cls.all.clear()
+        with open(path, 'r', encoding='windows-1251') as csvfile:
+            file_csv = csv.DictReader(csvfile, delimiter=',')
+            [cls(row["name"], float(row["price"]), int(row["quantity"])) for row in file_csv]
+
+    @staticmethod
+    def string_to_number(value: str) -> int:
+        """
+        Cтатический метод, возвращающий число из числа-строки.
+        """
+        return int(float(value))
