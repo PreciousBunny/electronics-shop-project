77a2dd3a1c2b7d5818eeb25c03285ce46611b400
Danila <icy.calmness@gmail.com>
2023-07-01 16:34:40 +0300

    add print() in homework_4.main.py

--- homework-4/main.py
+++ homework-4/main.py
@@ -3,15 +3,20 @@ from src.phone import Phone
 
 if __name__ == '__main__':
 
-    # смартфон iPhone 14, цена 120_000, количество товара 5, симкарт 2
+    # смартфон iPhone 14, цена 120_000, количество товара 5, сим-карт 2
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert str(phone1) == 'iPhone 14'
+    print(str(phone1))
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+    print(repr(phone1))
     assert phone1.number_of_sim == 2
+    print(phone1.number_of_sim)
 
     item1 = Item("Смартфон", 10000, 20)
     assert item1 + phone1 == 25
+    print(item1 + phone1)
     assert phone1 + phone1 == 10
+    print(phone1 + phone1)
 
     phone1.number_of_sim = 0
     # ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.
